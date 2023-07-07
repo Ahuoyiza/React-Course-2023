9ab4b3bed66bce9da56356bfcf4dd3362191cebd
Oluwatosin <hezealice@gmail.com>
2023-07-07 20:04:36 +0100

    first request

--- Class Activities/search-app/src/App.js
+++ Class Activities/search-app/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+
+const listItems = [
+  { name: "spder-boy" },
+  { name: "spder-man" },
+  { name: "spder-punk" },
+  { name: "spider-girl" },
+];
+
+const showList = () => {
+  console.log("stuff");
+};
 
 function App() {
   return (
     <div className="App">
-       Welcome to the spiderverse, search and find your favourite spiderman name!
+      <h1> Welcome to the spiderverse, view the spidey list</h1>
+      <button onClick={showList}>click me and show the button</button>
+      <ul>
+        {listItems.map((item) => {
+          <li>{item.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
