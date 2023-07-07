7fd37148f7973e4037434b52c13dbd3915c19262
BABABUSOLA <queenbusola1@gmail.com>
2023-07-07 20:11:30 +0100

    first commit

--- Class Activities/search-app/src/App.js
+++ Class Activities/search-app/src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
+const listItems = [
+  { name: "spider-boy" },
+  { name: "spider-man" },
+  { name: "spider-punk" },
+  { name: "spider-girl" },
+];
+
+const showList = () => {
+  console.log(listItems);
+};
 function App() {
   return (
     <div className="App">
-       Welcome to the spiderverse, search and find your favourite spiderman name!
+      <h1>Welcome to the spiderverse, view the spiderman list!</h1>
+      <button onClick={showList}>Click me and check the console</button>
+      <ul>
+        {listItems.map((item) => {
+          return <li>{item.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
