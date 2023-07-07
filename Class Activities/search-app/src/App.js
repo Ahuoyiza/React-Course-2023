1d809c8f1562f8a58ec7c49abb24dc05bb178e27
DevTinaTi <tinadesk8@gmail.com>
2023-07-07 20:03:16 +0100

    add

--- Class Activities/search-app/src/App.js
+++ Class Activities/search-app/src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const List = [
+  {
+    name: "spider-man",
+  },
+  {
+    name: "spider-girl",
+  },
+  {
+    name: "spider-hulk",
+  },
+  {
+    name: "spider-boy",
+  },
+];
 
 function App() {
+  function listFunction(List) {
+List.map(list)
+  }
   return (
     <div className="App">
-       Welcome to the spiderverse, search and find your favourite spiderman name!
+      <h1>
+        Welcome to the spiderverse, search and find your favourite spiderman
+        name!
+      </h1>
+      <button onClick={listFunction}>List</button>
     </div>
   );
 }
