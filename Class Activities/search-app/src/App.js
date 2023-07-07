1bb3dde6d6e9ed7216fd8a3212eb071e222c42cd
Gelaraph <angelaraph12@gmail.com>
2023-07-07 20:02:36 +0100

    first class assessment

--- Class Activities/search-app/src/App.js
+++ Class Activities/search-app/src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const listItems = [
+  { name: "spider-boy" },
+  {
+    name: "spider-man",
+  },
+  {
+    name: "spider-punk",
+  },
+  {
+    name: "spider-girl",
+  },
+];
+const showList = () => {
+  console.log("stuff");
+};
 
 function App() {
   return (
     <div className="App">
-       Welcome to the spiderverse, search and find your favourite spiderman name!
+      <h1>
+        Welcome to the spiderverse, search and find your favourite spiderman
+        name!
+      </h1>
+      <button onClick={showList}>Click me and check the console</button>
+      <ul>
+        {listItems.map((item) => {
+          return <li>{item.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
