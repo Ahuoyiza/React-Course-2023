ea5a9692266a7352deb472f1b643c46474bf7fa2
https://github.com/PeaElorm/test_demo.git <elormavugla7@gmail.com>
2023-07-08 21:25:42 +0000

    search functionality

--- Class Activities/search-app/src/App.js
+++ Class Activities/search-app/src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import Layout from './Layout';
-import {useState} from 'react';
+import "./App.css";
+import Layout from "./Layout";
+import { useState } from "react";
 
 const spiderPeople = [
   "Miles Morales",
@@ -9,46 +9,45 @@ const spiderPeople = [
   "Peni Parker",
   "Spider-Ham",
   "Spider-Man Noir",
-  "Spider-Man 2099 (Miguel O'Hara)",
-  "Spider-Punk (Hobie Brown)",
-  "Spider-Man (Aaron Davis)",
-  "Spider-Girl (Anya Corazon)"
+  "Spider-Man 2099 (Miquel O'Hara)",
+  "Spider Punk (Hobbie Brown)",
+  "Spider-Man (Aron Davis)",
+  "spider Girl (Anya Corazon)",
 ];
+
 function App() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [filteredItems, setFilteredItems] = useState([]);
 
-  const handleSearch = (e) =>{
+  const handleSearch = (e) => {
     const searchTerm = e.target.value;
     setSearchTerm(searchTerm);
 
-    const filteredItems = spiderPeople.filter((item)=>
-    item.toLowerCase().includes(searchTerm.toLowerCase())
+    const filteredItems = spiderPeople.filter((item) =>
+      item.toLowerCase().includes(searchTerm.toLowerCase())
     );
     setFilteredItems(filteredItems);
   };
   return (
     <div className="App">
-      <Layout title= "Spiderverse" nav1 = "Store" nav2= "Tokens" nav3= "Contact us"/>
-      <h1>Welcome to the spiderverse</h1>
-      <p>Search and find your spider people that were in
-        accross the spiderverse
-      </p>
+      <Layout title="Spiderverse" Nav1="Home" Nav2="Contact" Nav3="Movies" />
+      <h1>Welcome to Spiderverse</h1>
+      <p>Search And find your favorite spider man character</p>
+
       <div>
         <input
-          type='search'
-          placeholder='type to search here...'
-          value= {searchTerm}
+          type="search"
+          placeholder="search here..."
+          value={searchTerm}
           onChange={handleSearch}
-        />
+        ></input>
       </div>
+
       <div>
         <ul>
-          {
-            filteredItems.map((item, index)=>(
-              <li key={index}>{item}</li>
-            ))
-          }
+          {filteredItems.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
         </ul>
       </div>
     </div>
