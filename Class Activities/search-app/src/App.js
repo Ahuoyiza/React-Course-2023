43d93ef8b25ae84ed2279fadfea87d29020e9c6c
Sophia Ahuoyiza Abubakar <60071500+Ahuoyiza@users.noreply.github.com>
2023-07-08 19:29:57 +0000

    adds search functionality

--- Class Activities/search-app/src/App.js
+++ Class Activities/search-app/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Layout from './Layout';
+import {useState} from 'react';
 
 const spiderPeople = [
   "Miles Morales",
@@ -13,9 +15,42 @@ const spiderPeople = [
   "Spider-Girl (Anya Corazon)"
 ];
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredItems, setFilteredItems] = useState([]);
+
+  const handleSearch = (e) =>{
+    const searchTerm = e.target.value;
+    setSearchTerm(searchTerm);
+
+    const filteredItems = spiderPeople.filter((item)=>
+    item.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredItems(filteredItems);
+  };
   return (
     <div className="App">
-      <h1>spiderverse</h1>
+      <Layout title= "Spiderverse" nav1 = "Store" nav2= "Tokens" nav3= "Contact us"/>
+      <h1>Welcome to the spiderverse</h1>
+      <p>Search and find your spider people that were in
+        accross the spiderverse
+      </p>
+      <div>
+        <input
+          type='search'
+          placeholder='type to search here...'
+          value= {searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
+      <div>
+        <ul>
+          {
+            filteredItems.map((item, index)=>(
+              <li key={index}>{item}</li>
+            ))
+          }
+        </ul>
+      </div>
     </div>
   );
 }
