7e50de363add4adb60ba5263c6c05a199da9b083
Ajiboso-Adeola <adecom112@gmail.com>
2023-07-10 17:42:06 +0100

    created a spiderverse list and a button that renders the list items on click'

--- Class Activities/search-app/src/App.js
+++ Class Activities/search-app/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const listItems = [
+  { name: "spider-boy" },
+  { name: "spider-man" },
+  { name: "spider-punk" },
+  { name: "spider-girl" },
+];
+const showList = () => {
+  console.log("webb");
+};
 
 function App() {
   return (
     <div className="App">
-       Welcome to the spiderverse, search and find your favourite spiderman name!
+      <h1>
+        Welcome to the spiderverse, search and find your favourite spiderman
+        name.
+      </h1>
+      <button onClick={showList}>Click me and check the console</button>
+      <ul>
+        {listItems.map((item) => (
+          <li>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
