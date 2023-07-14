9b0207b2eb9a5edf80636ac59b39d15e94081ebc
Sophia Ahuoyiza Abubakar <60071500+Ahuoyiza@users.noreply.github.com>
2023-07-14 18:59:14 +0000

    adds routes and reg comp

--- Class Activities/multi-step-form/src/App.js
+++ Class Activities/multi-step-form/src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
+import {Component} from 'react';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import SuccessPage from './components/SuccessPage';
+import RegistrationForm from './components/RegistrationForm';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>multi step form</h1>
-    </div>
-  );
+
+class App extends Component {
+  render(){
+    return(
+      <Router>
+        <h1>Welcome to Femcode Africa</h1>
+        <p>React Bootcamp to start tomorrow</p>
+        <Link to = "/register">Click here to Register</Link>
+        <Routes>
+          <Route exact path = "/register" Component={RegistrationForm} />
+          <Route path= "/success" Component = {SuccessPage} />
+        </Routes>
+          
+      </Router>
+    )
+  }
 }
 
 export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>multi step form</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ Class Activities/multi-step-form/src/components/SuccessPage.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class SuccessPage extends Component{
+    render(){
+        return(
+            <>
+             <h1>You have been successfully Registered</h1>
+            </>
+        )
+    }
+}
+export default SuccessPage;
