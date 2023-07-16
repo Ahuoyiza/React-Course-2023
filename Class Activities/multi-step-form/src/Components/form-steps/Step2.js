2152e93fe69c9f2b2728bb6a909b0233e49a0725
BABABUSOLA <queenbusola1@gmail.com>
2023-07-16 18:46:07 +0100

    form step 2

--- Class Activities/multi-step-form/src/Components/form-steps/Step2.js
+++ Class Activities/multi-step-form/src/Components/form-steps/Step2.js
@@ -2,10 +2,28 @@ import React, { Component } from "react";
 
 class Step2 extends Component {
   render() {
+    const { formData, handleChange } = this.props;
     return (
       <>
         <h1>Step 2 </h1>
-        {/* <h4>Register to join the cohort</h4> */}
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          ></input>
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          ></input>
+        </label>
       </>
     );
   }
