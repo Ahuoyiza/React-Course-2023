3b5edecaac7df12630d5c3f2d321a65f80dc7bbb
BABABUSOLA <queenbusola1@gmail.com>
2023-07-16 18:58:59 +0100

    update the App.js with the functions

--- Class Activities/multi-step-form/src/App.js
+++ Class Activities/multi-step-form/src/App.js
@@ -11,13 +11,43 @@ import Step1 from "./components/form-steps/Step1";
 import Step2 from "./components/form-steps/Step2";
 import Step3 from "./components/form-steps/Step3";
 class App extends Component {
+  state = {
+    step: 1,
+    FormData: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      address: "",
+      city: "",
+      country: "",
+    },
+  };
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState((prevState) => ({
+      FormData: {
+        ...prevState.formData,
+        [name]: value,
+      },
+    }));
+  };
+
+  nextStep = () => {
+    this.setState((prevState) => ({
+      step: prevState.step + 1,
+    }));
+  };
+
+  prevStep = () => {
+    this.setState((prevState) => ({
+      step: prevState.step + 1,
+    }));
+  };
   render() {
+    const { step, formData } = this.state;
     return (
       <Router>
-        {/* <div>
-          <RegistrationForm />
-          <SuccessPage />
-        </div> */}
         <h1>Welcome to Femcode Africa</h1>
         <p>React Bootcamp to start tomorrow</p>
         <Link to="/register">Click here to register for the</Link>
--- Class Activities/multi-step-form/src/Components/form-steps/Step3.js
+++ Class Activities/multi-step-form/src/Components/form-steps/Step3.js
@@ -2,10 +2,37 @@ import React, { Component } from "react";
 
 class Step3 extends Component {
   render() {
+    const { formData, handleChange } = this.props;
     return (
       <>
         <h1>Step 3 </h1>
-        {/* <h4>Register to join the cohort</h4> */}
+        <label>
+          Address:
+          <input
+            type="text"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+          ></input>
+        </label>
+        <label>
+          City:
+          <input
+            type="text"
+            name="city"
+            value={formData.city}
+            onChange={handleChange}
+          ></input>
+        </label>
+        <label>
+          Country:
+          <input
+            type="text"
+            name="country"
+            value={formData.country}
+            onChange={handleChange}
+          ></input>
+        </label>
       </>
     );
   }
