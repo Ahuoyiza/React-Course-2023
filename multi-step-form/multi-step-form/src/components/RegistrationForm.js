9db25eee5f9905af2552978c8281d466ca1d1618
Mercy Kalu <belrahj3@gmail.com>
2023-07-16 07:16:33 +0100

    new project multi-step-form

---
+++ multi-step-form/multi-step-form/src/components/RegistrationForm.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class RegistrationForm extends Component{
+    render(){
+        return(
+            <>
+             <h1>Registration Form</h1>
+            </>
+        )
+    }
+}
+export default RegistrationForm;
