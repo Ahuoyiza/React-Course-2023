37c0799d4f21454bd4d376a29ef272dd7f959911
BABABUSOLA <queenbusola1@gmail.com>
2023-07-16 21:07:26 +0100

    change to functional component

--- Class Activities/multi-step-form/src/Components/SuccessPage.js
+++ Class Activities/multi-step-form/src/Components/SuccessPage.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 
-class SuccessPage extends Component {
-  render() {
-    return (
-      <div>
-        <h1>SuccessPage</h1>
-      </div>
-    );
-  }
-}
+const SuccessPage = () => {
+  return (
+    <div>
+      <h1>SuccessPage</h1>
+    </div>
+  );
+};
 
 export default SuccessPage;
--- Class Activities/multi-step-form/src/Components/form-steps/Step1.js
+++ Class Activities/multi-step-form/src/Components/form-steps/Step1.js
@@ -1,33 +1,16 @@
-import React, { Component } from "react";
+import { useState } from "react";
 
-class Step1 extends Component {
-  render() {
-    const { formData, handleChange } = this.props;
-    return (
-      <>
-        <h1>Welcome to Femcode </h1>
-        <h4>Register to join the cohort</h4>
-        <label>
-          First Name:
-          <input
-            type="text"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
-          ></input>
-        </label>
-        <label>
-          Last Name:
-          <input
-            type="text"
-            name="firstName"
-            value={formData.lastName}
-            onChange={handleChange}
-          ></input>
-        </label>
-      </>
-    );
-  }
-}
-
+const Step1 = ({}) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [course, setCourse] = useState("");
+  const [errors, setErrors] = useState({});
+  return (
+    <>
+      <h1>Step 1: Bio Data</h1>
+    </>
+  );
+};
 export default Step1;
