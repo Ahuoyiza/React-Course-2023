c78e39ad9d1e34dfac2d856b3d6c0e0fb04ed83b
Nechey <nechemetu@gmail.com>
2023-07-21 19:04:12 +0100

    update

--- Class Activities/search-app/src/App.js
+++ Class Activities/search-app/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import Layout from "./Layout";
 
+const spiderPeople = [
+  "Miles Morales",
+  "Peter  B. Parker",
+  "Gwen Stacy",
+  "Peni Parker",
+  "Spider-Ham",
+  "Spider-Man Noir",
+  "Spider-Man 2099 (Miguel O'Hara)",
+  "Spider-Punk (Hobie Brown)",
+  "Spider-Man (Aaron Davis)",
+  "Spider-Girl (Anya Corazon),",
+];
 function App() {
   return (
     <div className="App">
-       Welcome to the spiderverse, search and find your favourite spiderman name!
+      <Layout />
     </div>
   );
 }
---
+++ Class Activities/search-app/src/Layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function layout() {
+  return (
+    <div>
+      <header>
+        <h4>Spiderverse</h4>
+        <nav>
+          <a href="#">token</a>
+          <a href="#">store</a>
+          <a href="#">movies</a>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default layout;
