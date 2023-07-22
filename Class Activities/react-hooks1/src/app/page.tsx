910da6285067c32a407f3820a07c5926ca521c6c
https://github.com/PeaElorm/test_demo.git <elormavugla7@gmail.com>
2023-07-22 14:28:20 +0000

    react hooks starter

---
+++ Class Activities/react-hooks1/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Hello world</h1>
+      <p>Excited to create my first app with Next js</p>
+    </main>
+  );
+}
