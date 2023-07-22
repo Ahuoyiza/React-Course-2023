0c615d372a5387fb283be8cf668df90ef1a93a0c
Theresa-o <62052443+Theresa-o@users.noreply.github.com>
2023-07-22 12:03:59 -0700

    updated mouseOver

--- Class Activities/react-hook/src/components/mouseOver.tsx
+++ Class Activities/react-hook/src/components/mouseOver.tsx
@@ -3,6 +3,7 @@ import {useRef} from 'react';
 const mouseOver = ({priTag, secTag, alt}) =>{
     const imageRef = useRef(null);
     return(
+        // to change images from Ptag to SecTag
         <img src='{priTag}' alt='{alt}' 
         onMouseOver={
             () => {
