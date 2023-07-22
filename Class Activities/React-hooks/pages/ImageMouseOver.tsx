c0103570f98e08356aadc68249057169717a21b0
Sophia Ahuoyiza Abubakar <60071500+Ahuoyiza@users.noreply.github.com>
2023-07-22 19:58:52 +0100

    Create ImageMouseOver.tsx

---
+++ Class Activities/React-hooks/pages/ImageMouseOver.tsx
@@ -0,0 +1,15 @@
+import MouseOver from '../src/MouseOver'
+const ImageMouseOver = () =>{
+  return(
+    <div>
+      <h1>useRef Example</h1>
+      <p>using image mouse over effect </p>
+      <MouseOver 
+        priImg=""
+        secImg=""
+        alt= "barbie"
+      />
+    </div>
+  )
+}
+export default ImageMouseOver
