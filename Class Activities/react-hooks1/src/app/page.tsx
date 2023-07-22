fe551bf9b4ba11dce31095e790c6f69986ac5aad
https://github.com/PeaElorm/test_demo.git <elormavugla7@gmail.com>
2023-07-22 19:24:50 +0000

    mouseover effect starter

--- Class Activities/react-hooks1/src/app/page.tsx
+++ Class Activities/react-hooks1/src/app/page.tsx
@@ -1,10 +1,28 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [inputText, setInputText] = useState("");
+  const [history, setHistory] = useState([]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello world</h1>
-      <p>Excited to create my first app with Next js</p>
+      <h1>React Hooks!</h1>
+      <input
+        type="text"
+        placeholder="enter text here"
+        onChange={(e) => {
+          setInputText(e.target.value);
+          setHistory([...history, e.target.value]);
+        }}
+      />
+      <div>{inputText}</div>
+
+      <ul>
+        {history.map((item, index) => {
+          return <div>{item}</div>;
+        })}
+      </ul>
     </main>
   );
 }
