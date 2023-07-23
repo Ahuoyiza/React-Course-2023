20ed93de0cb8419fb7e10dcf7ae879f1d4f6cb63
Zawjahabdmaalik <ruqayyahalabi536@gmail.com>
2023-07-23 19:02:38 +0100

    react-hook

---
+++ Class-Activities/conf-app/src/app/page.tsx
@@ -0,0 +1,31 @@
+
+"use client"
+import { useState } from "react"
+
+
+export default function Home() {
+  const [input, setInput] = useState('')
+  const [inputHistory, setInputHistory] = useState<string[]>([]);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Hello World</h1>
+            <input
+            value={input}
+            placeholder="Enter you text here"
+            onChange={(e)=>{
+              const currentInput = e.target.value;
+              setInput(currentInput)
+              setInputHistory([...inputHistory, currentInput])
+            }}
+            />
+            <div>New input history</div>
+            <button>new input</button>
+            <ul>
+              {inputHistory.map((item, index)=>(
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+
+    </main>
+  )
+}
